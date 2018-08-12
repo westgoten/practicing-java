3a7bb13f15414238d1526fd0771b0ca31fb3a99e
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2018-08-12 14:56:03 -0300

    User interface and Input handling are done

--- product-inventory/inventory/Main.java
+++ product-inventory/inventory/Main.java
@@ -3,12 +3,12 @@ package inventory;
 import inventory.management.Manager;
 import inventory.storage.Product;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		//Work In Progress
 		Scanner in = new Scanner(System.in).useLocale(Locale.US);
 		
 		while (true) {
@@ -29,33 +29,36 @@ public class Main {
 				if (option >= 1 && option <= 4)
 					invalid = false;
 				else {
-					System.out.println("\nInvalid option"); //Pause for 5s-10s
+					System.out.println("\nInvalid option");
 					invalid = true;
+					waitSeconds(1);
 				}
 			} while (invalid);
 			
 			switch (option) {
 				case 1:
-					addProduct(in);
+					addProduct();
 					break;
 				case 2:
-					removeProduct(in);
+					removeProduct();
 					break;
 				case 3:
-					modifyProductQuantity(in);
+					modifyProductQuantity();
 					break;
 				case 4:
-					listAllProducts(in);
+					listAllProducts();
 					break;
 			}
 		}
 	}
 	
-	private static void addProduct(Scanner in) throws FileNotFoundException, IOException {
+	private static void addProduct() throws FileNotFoundException, IOException {
 		String pName = "";
 		double pPrice = 0.0;
 		int pQuantity = 0;
 		boolean invalid = false;
+		
+		Scanner in = new Scanner(System.in).useLocale(Locale.US);
 		
 		System.out.println("\nAdd a new product");
 		
@@ -109,19 +112,23 @@ public class Main {
 		} while (invalid);
 		
 		if (Manager.hasProduct(pName)) {
-			System.out.println("\nA product with that name already exists"); // Pause for 10s
+			System.out.println("\nA product with that name already exists");
+			waitSeconds(3);
 		} else {
 			Manager.addProduct(pName, pPrice, pQuantity);
-			System.out.println("\nAdded '" + pName.toUpperCase() + "' to the inventory"); // Pause for 10s
+			System.out.println("\nAdded '" + pName.toUpperCase() + "' to the inventory");
+			waitSeconds(2);
 		}
 		
 		return;
 	}
 	
-	private static void removeProduct(Scanner in) throws FileNotFoundException, IOException {
+	private static void removeProduct() throws FileNotFoundException, IOException {
 		int pID = 0;
 		String pName = "";
 		boolean invalid = false;
+		
+		Scanner in = new Scanner(System.in).useLocale(Locale.US);
 		
 		System.out.println("\nRemove a product");
 		
@@ -130,18 +137,22 @@ public class Main {
 		
 		if (Manager.hasProduct(pID)) {
 			pName = Manager.removeProduct(pID);
-			System.out.println("\nRemoved '" + pName + "' from the inventory"); // Pause for 10s
+			System.out.println("\nRemoved '" + pName + "' from the inventory");
+			waitSeconds(3);
 		} else {
-			System.out.println("\nA product with that ID number doesn't exist"); // Pause for 10s
+			System.out.println("\nA product with that ID number doesn't exist");
+			waitSeconds(3);
 		}
 		
 		return;
 	}
 	
-	private static void modifyProductQuantity(Scanner in) throws FileNotFoundException, IOException {
+	private static void modifyProductQuantity() throws FileNotFoundException, IOException {
 		int pID = 0, pQuantity = 0;
 		String pName = "";
 		boolean invalid = false;
+		
+		Scanner in = new Scanner(System.in).useLocale(Locale.US);
 		
 		System.out.println("\nModify product quantity");
 		
@@ -162,20 +173,24 @@ public class Main {
 		
 		if (Manager.hasProduct(pID)) {
 			pName = Manager.modifyProductQuantity(pID, pQuantity);
-			System.out.println("\nThere is " + pQuantity + " units of '" + pName + "' in the inventory"); // Pause for 10s
+			System.out.println("\nThere is " + pQuantity + " units of '" + pName + "' in the inventory");
+			waitSeconds(3);
 		} else {
-			System.out.println("\nA product with that ID number doesn't exist"); // Pause for 10s
+			System.out.println("\nA product with that ID number doesn't exist");
+			waitSeconds(3);
 		}
 		
 		return;
 	}
 	
-	private static void listAllProducts(Scanner in) {
+	private static void listAllProducts() {
 		int option = 0;
 		boolean invalid = false;
 		
+		Scanner in = new Scanner(System.in).useLocale(Locale.US);
+		
 		System.out.println("\nList of products\n\nDo you want to sort the list by:");
-		System.out.println("\n1) ID number"+
+		System.out.println("\n1) ID Number"+
 							"\n2) Name"+
 							"\n3) Price"+
 							"\n4) Quantity");
@@ -188,23 +203,36 @@ public class Main {
 			if (option >= 1 && option <= 4)
 				invalid = false;
 			else {
-				System.out.println("\nInvalid option"); //Pause for 5s-10s
+				System.out.println("\nInvalid option");
 				invalid = true;
+				waitSeconds(1);
 			}
 		} while (invalid);
 		
 		Manager.sortProductList(option);
 		
-		System.out.println("\n| ID Number | Name | Price | Quantity |");
+		System.out.printf("\n| ID Number  | %-25s | %-12s | Quantity |\n\n", "Name", "Price (U$)");
 		
 		ArrayList<Product> productList = Manager.getProductList();
 		for (Product p : productList) {
-			System.out.printf(Locale.US, "| %04d | %s | %.2f | %d |\n", p.getID(), p.getName(), p.getPrice(), p.getQuantity());
+			System.out.printf(Locale.US, "|    %04d    | %-25s | %-12.2f | %-8d |\n", p.getID(), p.getName(), p.getPrice(), p.getQuantity());
 		}
-
-		System.out.print("\nPress ENTER to return to Menu...");
-		in.nextLine(); //Fix this
+		System.out.printf(Locale.US, "\n| Inventory Value ------------------------------------- U$ %.2f |\n", Manager.getInventoryValue());
 		
+		System.out.print("\nPress ENTER to return to Menu...");
+		in.nextLine();
+		in.nextLine();
+		
+		return;
+	}
+	
+	private static void waitSeconds(int sec) {
+		try{
+			TimeUnit.SECONDS.sleep(sec);
+			} catch (InterruptedException e) {
+				System.err.println("InterruptedException: " + e.getMessage());
+			}
+			
 		return;
 	}
 }
