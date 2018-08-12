7282cb39994c8a86d99ff175e947b489ad0404a2
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2018-08-11 23:45:42 -0300

    Changed Main and Manager classes

--- product-inventory/inventory/Main.java
+++ product-inventory/inventory/Main.java
@@ -2,9 +2,209 @@ package inventory;
 
 import inventory.management.Manager;
 import inventory.storage.Product;
+import java.util.*;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
 		//Work In Progress
+		Scanner in = new Scanner(System.in).useLocale(Locale.US);
+		
+		while (true) {
+			int option = 0;
+			boolean invalid = false;
+			
+			System.out.println("\nWelcome to the Inventory Manager!");
+			System.out.println("\n1) Add a new product"+
+								"\n2) Remove a product"+
+								"\n3) Modify product quantity"+
+								"\n4) List all products");
+			
+			do {
+				System.out.print("\nChoose one of the options above (1-4): ");
+			
+				option = in.nextInt();
+				
+				if (option >= 1 && option <= 4)
+					invalid = false;
+				else {
+					System.out.println("\nInvalid option"); //Pause for 5s-10s
+					invalid = true;
+				}
+			} while (invalid);
+			
+			switch (option) {
+				case 1:
+					addProduct(in);
+					break;
+				case 2:
+					removeProduct(in);
+					break;
+				case 3:
+					modifyProductQuantity(in);
+					break;
+				case 4:
+					listAllProducts(in);
+					break;
+			}
+		}
+	}
+	
+	private static void addProduct(Scanner in) throws FileNotFoundException, IOException {
+		String pName = "";
+		double pPrice = 0.0;
+		int pQuantity = 0;
+		boolean invalid = false;
+		
+		System.out.println("\nAdd a new product");
+		
+		do {
+			System.out.print("\nProduct's name: ");
+			pName = in.nextLine();
+			
+			int i = 0;
+			int countWhitespace = 0;
+			int len = pName.length();
+			for (i = 0; i < len; i++) {
+				if (!(Character.isAlphabetic(pName.codePointAt(i)) || Character.isWhitespace(pName.codePointAt(i)))) {
+					System.out.println("\nInvalid name");
+					invalid = true;
+					break;
+				} else if (Character.isWhitespace(pName.codePointAt(i))) {
+					if (++countWhitespace > 1) {
+						System.out.println("\nInvalid name");
+						invalid = true;
+						break;
+					}
+				}
+			}
+			if (i == len) {
+				invalid = false;
+			}
+		} while (invalid);
+		
+		do {
+			System.out.print("\nProduct's price: ");
+			pPrice = in.nextDouble();
+			
+			if(Manager.isPriceValid(pPrice)) {
+				invalid = false;
+			} else {
+				System.out.println("\nInvalid price");
+				invalid = true;
+			}
+		} while (invalid);
+		
+		do {
+			System.out.print("\nProduct's quantity: ");
+			pQuantity = in.nextInt();
+			
+			if(Manager.isQuantValid(pQuantity)) {
+				invalid = false;
+			} else {
+				System.out.println("\nInvalid quantity");
+				invalid = true;
+			}
+		} while (invalid);
+		
+		if (Manager.hasProduct(pName)) {
+			System.out.println("\nA product with that name already exists"); // Pause for 10s
+		} else {
+			Manager.addProduct(pName, pPrice, pQuantity);
+			System.out.println("\nAdded '" + pName.toUpperCase() + "' to the inventory"); // Pause for 10s
+		}
+		
+		return;
+	}
+	
+	private static void removeProduct(Scanner in) throws FileNotFoundException, IOException {
+		int pID = 0;
+		String pName = "";
+		boolean invalid = false;
+		
+		System.out.println("\nRemove a product");
+		
+		System.out.print("\nProduct's ID number: ");
+		pID = in.nextInt();
+		
+		if (Manager.hasProduct(pID)) {
+			pName = Manager.removeProduct(pID);
+			System.out.println("\nRemoved '" + pName + "' from the inventory"); // Pause for 10s
+		} else {
+			System.out.println("\nA product with that ID number doesn't exist"); // Pause for 10s
+		}
+		
+		return;
+	}
+	
+	private static void modifyProductQuantity(Scanner in) throws FileNotFoundException, IOException {
+		int pID = 0, pQuantity = 0;
+		String pName = "";
+		boolean invalid = false;
+		
+		System.out.println("\nModify product quantity");
+		
+		System.out.print("\nProduct's ID number: ");
+		pID = in.nextInt();
+		
+		do {
+			System.out.print("\nProduct's new quantity: ");
+			pQuantity = in.nextInt();
+			
+			if(Manager.isQuantValid(pQuantity)) {
+				invalid = false;
+			} else {
+				System.out.println("\nInvalid quantity");
+				invalid = true;
+			}
+		} while (invalid);
+		
+		if (Manager.hasProduct(pID)) {
+			pName = Manager.modifyProductQuantity(pID, pQuantity);
+			System.out.println("\nThere is " + pQuantity + " units of '" + pName + "' in the inventory"); // Pause for 10s
+		} else {
+			System.out.println("\nA product with that ID number doesn't exist"); // Pause for 10s
+		}
+		
+		return;
+	}
+	
+	private static void listAllProducts(Scanner in) {
+		int option = 0;
+		boolean invalid = false;
+		
+		System.out.println("\nList of products\n\nDo you want to sort the list by:");
+		System.out.println("\n1) ID number"+
+							"\n2) Name"+
+							"\n3) Price"+
+							"\n4) Quantity");
+		
+		do {
+			System.out.print("\nChoose an option (1-4): ");
+			
+			option = in.nextInt();
+				
+			if (option >= 1 && option <= 4)
+				invalid = false;
+			else {
+				System.out.println("\nInvalid option"); //Pause for 5s-10s
+				invalid = true;
+			}
+		} while (invalid);
+		
+		Manager.sortProductList(option);
+		
+		System.out.println("\n| ID Number | Name | Price | Quantity |");
+		
+		ArrayList<Product> productList = Manager.getProductList();
+		for (Product p : productList) {
+			System.out.printf(Locale.US, "| %04d | %s | %.2f | %d |\n", p.getID(), p.getName(), p.getPrice(), p.getQuantity());
+		}
+
+		System.out.print("\nPress ENTER to return to Menu...");
+		in.nextLine(); //Fix this
+		
+		return;
 	}
 }
--- product-inventory/inventory/management/Manager.java
+++ product-inventory/inventory/management/Manager.java
@@ -12,90 +12,146 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Manager {
-	/** 
-	 * I need to find a way to store/read products' data.
-	 * Solution:
-	 *   Create an arraylist of objects and store in a file
-	 */
 
 	private static File file = Paths.get(System.getProperty("user.dir"), "inventory", "data", "product_list.txt").toFile();
-	private static ArrayList<Product> product_list;
+	private static ArrayList<Product> productList;
 	
 	static {
-		if (file.length() > 0) {
-			getFileData();
-		} else {
-			product_list = new ArrayList<Product>();
+		try{
+			if (file.length() > 0) {
+				getFileData();
+			} else {
+				productList = new ArrayList<Product>();
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("FileNotFoundException: " + e.getMessage());
+		} catch (IOException e) {
+			System.err.println("IOException: " + e.getMessage());
+		} catch (ClassNotFoundException e) {
+			System.err.println("ClassNotFoundException: " + e.getMessage());
 		}
 	}
 	
+	public static ArrayList<Product> getProductList() {
+		return productList;
+	}
+	
+	public static boolean hasProduct(String name) {
+		name = name.toUpperCase();
+		
+		for (Product p : productList) {
+			if (p.getName().equals(name))
+				return true;
+		}
+		
+		return false;
+	}
+	
+	public static boolean hasProduct(int id) {
+		if (id <= Product.getNumOfProducts() && id > 0)
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean isPriceValid(double price) {
+		if (price >= 0.0)
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean isQuantValid(int quantity) {
+		if (quantity >= 0)
+			return true;
+		else
+			return false;
+	}
+	
 	public static void addProduct(String name, double price, int quantity) throws FileNotFoundException, IOException {
+		name = name.toUpperCase();
+		
 		Product aProduct = new Product(name, price, quantity);
-		product_list.add(aProduct);
+		productList.add(aProduct);
 		setFileData();
 		
 		return;
 	}
 	
-	public static void removeProduct(int id) throws FileNotFoundException, IOException {
-		int listSize = product_list.size();
+	public static String removeProduct(int id) throws FileNotFoundException, IOException {
+		int listSize = productList.size();
+		String removedProductName = "";
 		
 		for (int i = 0; i < listSize; i++) {
-			if (product_list.get(i).getID() == id) {
-				product_list.remove(i);
-				product_list.trimToSize();
+			if (productList.get(i).getID() == id) {
+				removedProductName = productList.get(i).getName();
+				productList.remove(i);
+				productList.trimToSize();
 				Product.setNumOfProducts(Product.getNumOfProducts() - 1);
 				listSize--;
 				
+				Product temp;
 				for (; i < listSize; i++) {
-					Product temp = product_list.get(i);
+					temp = productList.get(i);
 					temp.setID(temp.getID() - 1);
-					temp = null;
 				}
+				temp = null;
 				
 				setFileData();
 				break;
 			}
 		}
 		
-		return;
+		return removedProductName;
 	}
 	
-	public static void modifyProductQuantity(int id, int quantity) throws FileNotFoundException, IOException {
-		for (Product p : product_list) {
+	public static String modifyProductQuantity(int id, int quantity) throws FileNotFoundException, IOException {
+		String productName = "";
+		
+		for (Product p : productList) {
 			if (p.getID() == id) {
+				productName = p.getName();
 				p.setQuantity(quantity);
 				setFileData();
 				break;
 			}
 		}
 		
-		return;
+		return productName;
 	}
 	
 	public static void sortProductList(int option) {
 		switch(option) {
 			case 1:
-				Collections.sort(product_list, Comparator.comparing(Product::getID));
+				Collections.sort(productList, Comparator.comparing(Product::getID));
 				break;
 			case 2:
-				Collections.sort(product_list, Comparator.comparing(Product::getName));
+				Collections.sort(productList, Comparator.comparing(Product::getName));
 				break;
 			case 3:
-				Collections.sort(product_list, Comparator.comparing(Product::getPrice));
+				Collections.sort(productList, Comparator.comparing(Product::getPrice));
 				break;
 			case 4:
-				Collections.sort(product_list, Comparator.comparing(Product::getQuantity));
+				Collections.sort(productList, Comparator.comparing(Product::getQuantity));
 				break;
 		}
 		
 		return;
 	}
 	
+	public static double getInventoryValue() {
+		double total = 0.0;
+		
+		for (Product p : productList)
+			total += p.getPrice() * p.getQuantity();
+		
+		return total;
+	}
+	
 	private static void getFileData() throws FileNotFoundException, IOException, ClassNotFoundException {
 		FileInputStream fis = new FileInputStream(file);
 		ObjectInputStream ois = new ObjectInputStream(fis);
-		product_list = (ArrayList<Product>) ois.readObject();
+		productList = (ArrayList<Product>) ois.readObject();
 		Product.setNumOfProducts(ois.readInt());
 		ois.close();
 		
@@ -105,7 +161,7 @@ public class Manager {
 	private static void setFileData() throws FileNotFoundException, IOException {
 		FileOutputStream fos = new FileOutputStream(file);
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(product_list);
+		oos.writeObject(productList);
 		oos.writeInt(Product.getNumOfProducts());
 		oos.close();
 		
